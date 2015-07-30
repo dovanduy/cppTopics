3155246caff849becb3d7151906aa3df05058eb0
kilarionov <k.ilarionov@gmail.com>
2015-07-30 15:34:27 +0300

    cleared init cases

--- Arraya/oneDimension/charArray/5.1.CHISLA.cpp
+++ Arraya/oneDimension/charArray/5.1.CHISLA.cpp
@@ -8,17 +8,18 @@ const char DELIMITER = ' ';
 bool isDigit (char c)
 {	return (c>='0' && c<='9') ;	}
 
-int digitValue(char c)
+int digitValue(char c)	// converts char digit to a number
 {	return ((int)(c-'0')) ;	}
 
 int main()
 {
 	char chars [QTYCHARS] ; 		// input variables as original.
 	int countNumbers = 0, sum=0, 	// output data 
-	power10, tmpNum ; 				// temporary variables
-	for (int k=0; k<QTYCHARS; k++) // to read data
+	k, power10, tmpNum ; 				// temporary variables
+	for (k=0; k<QTYCHARS; k++) // to read data
 		cin >>chars[k] ;
-	for (int k = QTYCHARS-1 ; k >= 0; k--) // reverse pass through an array
+	for (k = QTYCHARS-1, sum  = 0, countNumbers = 0 ; k >= 0; k--) 
+	// The loop does a reverse pass through an array.
 	  if (isDigit(chars[k]))
 		{
 			for(power10 = 1, tmpNum=0, countNumbers++ ; 

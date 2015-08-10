c1ff7fbf865e87e783383c919755d42b96ab9309
kilarionov <k.ilarionov@gmail.com>
2015-08-10 14:53:59 +0300

    Custom made FORMULA + IF, very basic example SUBS-01.BOOK

---
+++ branchingLogic/IF/SUBS-01.BOOK-CustomFormulaIF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long totalPages, campDays, pagesDayly,
+		worksDays, totalPagesMonthly, totalMonths ;
+	cin >>totalPages >>campDays >>pagesDayly;
+	if (campDays == 30 || pagesDayly == 0)
+	{
+		cout <<"never" ;
+		return 0 ;
+	}
+	
+	worksDays	 =	30-campDays ;
+	totalPagesMonthly = (worksDays*pagesDayly) ;
+	totalMonths = totalPages/totalPagesMonthly +
+	 			 ((0==totalPages%totalPagesMonthly) ? 0 : 1) ;
+
+	cout <<totalMonths/12 <<" years " <<totalMonths%12 <<" months";
+	return 0;
+}

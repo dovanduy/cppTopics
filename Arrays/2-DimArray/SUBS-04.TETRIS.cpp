cb0b7e736359514dcbf0ead7d5ba5c85bc8f2e51
kilarionov <k.ilarionov@gmail.com>
2015-08-10 12:08:33 +0300

    Dynamic 2 Dimensional C++ Array

--- Arrays/2-DimArray/SUBS-04.TETRIS.cpp
+++ Arrays/2-DimArray/SUBS-04.TETRIS.cpp
@@ -6,6 +6,43 @@ using namespace std;
 bool** gameField = NULL ; // empty array
 short redove = 0, coloni = 0, row, col ;
 string s;
+int counterI=0, counterL=0, counterJ=0, counterO=0, counterZ=0, counterS=0, counterT=0 ;
+
+void checkI (short r, short c)
+{
+	if (gameField[r][c] && gameField[r+1][c] && gameField[r+2][c] && gameField[r+3][c])
+		counterI++ ;
+}
+void checkL (short r, short c)
+{
+	if (gameField[r][c] && gameField[r+1][c] && gameField[r+2][c] && gameField[r+2][c+1])
+		counterL++ ;
+}
+void checkJ (short r, short c)
+{
+	if (gameField[r][c] && gameField[r+1][c] && gameField[r+2][c] && gameField[r+2][c-1])
+		counterJ++ ;
+}
+void checkO (short r, short c)
+{
+	if (gameField[r][c] && gameField[r+1][c] && gameField[r+1][c+1] && gameField[r][c+1])
+		counterO++ ;
+}
+void checkZ (short r, short c)
+{
+	if (gameField[r][c] && gameField[r][c+1] && gameField[r+1][c+1] && gameField[r+1][c+2])
+		counterZ++ ;
+}
+void checkS (short r, short c)
+{
+	if (gameField[r+1][c] && gameField[r+1][c+1] && gameField[r][c+1] && gameField[r][c+2])
+		counterS++ ;
+}
+void checkT (short r, short c)
+{
+	if (gameField[r][c] && gameField[r][c+1] && gameField[r+1][c+1] && gameField[r][c+2])
+		counterT++ ;
+}
 
 main ()
 { // Dynamically Allocated 2 Dimensional Array.
@@ -22,16 +59,25 @@ main ()
 	for (row=2; row<redove+2; row++)
 	  for (cin >>s, col=2; col<coloni+2; col++)
 	  { // input data and set up a gameField
-	  	if (s[col]=='o')
+	  	if (s[col-2]=='o')
 	  		gameField[row][col] = true ;
 	  } ;
 
-	for (row=2; row<redove+2; row++, cout <<endl)
+	for (row=2; row<redove+2; cout <<endl, row++)
 	  for (col=2; col<coloni+2; col++)
 	  {
-	  	// cout <<gameField[row][col] ; 
+	  	 //cout <<gameField[row][col] ;
+	  	checkI(row, col) ;
+	  	checkL(row, col) ;
+	  	checkJ(row, col) ;
+	  	checkO(row, col) ;
+	  	checkZ(row, col) ;
+	  	checkS(row, col) ;
+	  	checkT(row, col) ;
 	  }
 
+	cout <<"I:"<<counterI <<", L:" <<counterL<<", J:" <<counterJ 
+	<<", O:"<<counterO <<", Z:"<<counterZ <<", S:"<<counterS <<", T:"<<counterT;
 ///*	
 	if (gameField != NULL) // if the array is not empty then
 	{ 	// to get memory back as a free, re-usable memory.

5ad7777e75c284e2aeb72d6ad461d1caf0dbc32b
kilarionov <k.ilarionov@gmail.com>
2015-08-21 14:33:07 +0300

    arc MaxBinNumber group-D

---
+++ group-D/D-JA-2009-2.MaxBinNumber-Algorithm.cpp
@@ -0,0 +1,19 @@
+// D-JA-2009-2.MaxBinNumber-Algorithm.cpp
+#include <iostream>
+// #include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool sortWay(char c1, char c2) 
+{ 
+	return c1 > c2;
+}
+
+main ()
+{ 
+	string binNum ;
+	cin >>binNum ;
+	sort(binNum.begin(), binNum.end(), sortWay) ; // algorithm
+	cout <<binNum<<endl;
+}

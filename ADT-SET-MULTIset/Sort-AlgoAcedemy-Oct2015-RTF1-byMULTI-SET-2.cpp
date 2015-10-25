4bb4cb92bb645fdacb11e22920219d7b7a62fa3a
kilarionov <k.ilarionov@gmail.com>
2015-10-25 20:25:53 +0200

    MULTI-SET+CustomSORT

--- ADT-SET-MULTIset/Sort-AlgoAcedemy-Oct2015-RTF1-byMULTI-SET-2.cpp
+++ ADT-SET-MULTIset/Sort-AlgoAcedemy-Oct2015-RTF1-byMULTI-SET-2.cpp
@@ -19,62 +19,59 @@ bool isZero (char c) { return c=='0' ; }
 struct cmp {
   bool operator() (const char& c1, const char& c2) const
   { // c1 <= c2
-  	if (isZero(&c2))
+  	if (isZero(c2))
 		return true ;
-	else if (isZero(*c1))
+	else if (isZero(c1))
 		return false ;
-    else if (isDigit(*c1) && isDigit(*c2))
+    else if (isDigit(c1) && isDigit(c2))
     	return c1<c2 ;
-  	else if (isCapital(*c1) && isLower(*c2))
-    	return c1 < c2-D ;
-    else if (isCapital(*c1) && isCapital(*c2))
-    	return c1<c2 ;
-    else if (isCapital(*c1) && isDigit(*c2))
+  	else if (isCapital(c1) && isLower(c2)
+	   		&& c1+D == c2)
     	return true ;
-    else if (isLower(*c1) && (! isDigit(*c2)))
+    else if (isCapital(c1) && isLower(c2))
+    	return c1+D<c2 ; 
+    else if (isCapital(c1) && isCapital(c2))
+    	return c1<c2 ;
+    else if (isCapital(c1) && isDigit(c2))
+    	return true ;
+    else if (isLower(c1) && (isCapital(c2))
+	         && (c1 == c2+D))
+		return false ;
+	else if (isLower(c1) && (isCapital(c2)))
     	return c1-D < c2 ;
-    else if (isLower(*c1) && isDigit(*c2))
+    else if (isLower(c1) && (isLower(c2)))
+    	return c1 <= c2 ;
+    else if (isLower(c1) && isDigit(c2))
     	return true ;
   }
 };
 
-// from set to MULTIset + same include used
+// customSORT  ? 20/100
 multiset<char, cmp> myGoodChars ; 
 
-void print (char c)
+bool isGoodChar (char c)
 {
-	for (auto it = myGoodChars.find(c);
-		(*it) == c && it != myGoodChars.end();
-		it++)
-		  cout <<c;
+	return c!='x' &&
+			(isLower(c) ||
+			 isCapital(c) ||
+			 isDigit(c) || isZero(c)) ;
 }
-
 int main ()
 {
 	cin >>n;
+	myGoodChars.clear() ;
 	for (k=0; k<n; k++)
 	{
 		cin >>asciiCode;
 		if  (asciiCode>=32 && asciiCode<=126)
 		{
 			c=(char)asciiCode;
-			if (c!='x' &&
-			((c>='a' && c<='z') ||
-			(c>='A' && c<='Z') ||
-			(c>='0' && c<='9')))
+			if (isGoodChar(c))
 				myGoodChars.insert(c) ;
 		}
 	} ;
-	for (char ch='A'; ch<='Z'; ch++)
-	{
-		print (ch);  // capital letter
-		print (ch+D); // lower letter
-	} ;
-	for (char ch='1'; ch<='9'; ch++)
-	{
-		print (ch) ;
-	} ;
-	print ('0') ;
+	for (auto it=myGoodChars.begin(); it!=myGoodChars.end(); ++it)
+		cout <<(*it) ;
 	cout <<endl;
 	return 0;
 }

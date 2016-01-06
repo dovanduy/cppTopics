40c90aa0a32c567f73053927543a8349147ca960
unknown <k.ilarionov@gmail.com>
2016-01-06 14:22:47 +0200

    minor

--- Arrays/oneDimension/charArray/SUBS-04.WeirdCombinations-StackCharArrayString.cpp
+++ Arrays/oneDimension/charArray/SUBS-04.WeirdCombinations-StackCharArrayString.cpp
@@ -1,8 +1,10 @@
 // SUBS-04.WeirdCombinations.cpp
 // https://judge.softuni.bg/Contests/37/CSharp-Basics-Exam-7-November-2014
 #include <iostream>
+// https://bg.wikipedia.org/wiki/%D0%A1%D1%82%D0%B5%D0%BA_(%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D0%B0_%D0%BE%D1%82_%D0%B4%D0%B0%D0%BD%D0%BD%D0%B8)
+// https://www.youtube.com/watch?v=5UjENioK8tw
 #include <stack>
-#include <string>
+#include <string> // Koala Press - SIMVOLNI NIZOVE - p 123.
 using namespace std;
 
 const int numSysBase = 5 ;

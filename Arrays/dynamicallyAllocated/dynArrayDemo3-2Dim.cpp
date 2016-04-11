bd87faaefea1f999db2b3e52b08459d108f692f6
kilarionov <k.ilarionov@gmail.com>
2016-04-11 10:28:55 +0300

    2-Dim-Array-Func-Pointers-Demo

---
+++ Arrays/dynamicallyAllocated/dynArrayDemo3-2Dim.cpp
@@ -0,0 +1,33 @@
+// Basic Example about 2-dimensional dynamic array (by pointers)
+// 2-Dim Matrix as a parameter
+#include <iostream>
+using namespace std;
+
+void printMatrix (int ** m, int r, int c)
+{
+	for (int i=0; i<r; ++i)
+	{
+	  for (int j=0; j<c; ++j)
+	    cout <<m[i][j]<<' ' ;
+	  cout <<endl;
+	}	
+}
+int main ()
+{
+	int red, col;
+	int **matrix ;
+	cin >>red >>col;
+	matrix = new int*[red]; // array of pointers to int vars
+// new statement allocates memory during prog execution
+// i.e. dynamicaly array alocation
+	for (int i=0; i<red; ++i)
+		matrix[i] = new int[col] ; 
+// matrix is array of 1-dim arrays
+	for (int i=0; i<red; ++i)
+	  for (int j=0; j<col; ++j)
+	    cin >>matrix[i][j] ;
+	cout <<"\n===\n";
+	printMatrix (matrix, red, col) ;
+// Here we can release memory (OP).
+	return 0;
+}

0d03f58d43ace16f618c193794c4f4bcda6126e6
kilarionov <k.ilarionov@gmail.com>
2016-06-11 09:03:27 +0300

    8-bitASCII

---
+++ Loops/8-bitCodeTable.c
@@ -0,0 +1,9 @@
+// To print 8-bit code table
+#include <cstdio>
+void main()
+{
+int i=0;
+for(i=0;i<256;i++)
+printf("%d %c\n",i,i);
+}
+
---
+++ Loops/8-bitCodeTable.cpp
@@ -0,0 +1,10 @@
+// To print 8-bit code table
+#include <cstdio>
+int main()
+{
+	int i=0;
+	for(i=0;i<256;i++)
+		printf("%d %c\n",i,i) ;
+	return 0;
+}
+

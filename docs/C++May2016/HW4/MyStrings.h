0b91e8f7e61432dd9ef37c4ee19b4dc566fa3dd0
kilarionov <k.ilarionov@gmail.com>
2016-06-26 21:17:52 +0300

    MyStrings.h

--- docs/C++May2016/HW4/MyStrings.h
+++ docs/C++May2016/HW4/MyStrings.h
@@ -1,6 +1,6 @@
 #include <string>
 #include <sstream>
-template <class Type>
+template <class Type> // Type is valid C++ numeric type
 class MyStrings : public string
 {
 public:
@@ -13,6 +13,16 @@ public:
       ss.clear(); //clear error flags
       //here asInt is an lon long int type now
 	return asInt ;
+	}
+	
+	string fromDecNumToString (Type decNum) {
+		string res;
+		stringstream myStream ;
+		myStream <<dec <<decNum ; // dec is a std::dec manipulator
+		res = myStream.str() ;
+		myStream.str(""); //clear the stringstream
+      	myStream.clear(); //clear error flags
+		return res ;
 	}
 	
 	bool isFileExist(const char* fileName)

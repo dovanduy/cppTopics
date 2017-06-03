f276ece3837f12c542f36676f6985a6e919ba67b
kilarionov <k.ilarionov@gmail.com>
2017-06-03 17:01:19 +0300

    TribonachiTriangle

---
+++ VS/CSharp/Hello/NextDay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// http://bgcoder.com/Contests/Practice/Index/41#0
+
+namespace NextDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            int d = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            DateTime mdate = new DateTime(y, m, d);
+            mdate=mdate.AddDays(1);
+            Console.WriteLine(mdate.Day+"."+mdate.Month +"."+mdate.Year);
+        }
+    }
+}
---
+++ VS/CSharp/Hello/TribonachiTriangle/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TribonachiTriangle
+{
+    class Program
+    {
+        static long t1, t2, t3;
+        static int n;
+        static long tribNum(int seqNum)
+        {
+            if (1 == seqNum)
+                return t1;
+            else if (2 == seqNum)
+                return t2;
+            else if (3 == seqNum) return t3;
+            return tribNum(seqNum - 1) + tribNum(seqNum - 2) + tribNum(seqNum - 3);
+        }
+        static void ver1Slow(string[] args)
+        {
+            t1 = long.Parse(Console.ReadLine());
+            t2 = long.Parse(Console.ReadLine());
+            t3 = long.Parse(Console.ReadLine());
+            n = int.Parse(Console.ReadLine());
+            int seqNum = 1;
+            for (int row = 1; row <= n; ++row)
+            {
+                for (int i = 1; i <= row; ++i)
+                {
+                    Console.Write("{0} ", tribNum(seqNum));
+                    seqNum++;
+                };
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            t1 = long.Parse(Console.ReadLine());
+            t2 = long.Parse(Console.ReadLine());
+            t3 = long.Parse(Console.ReadLine());
+            long tNum ;
+            n = int.Parse(Console.ReadLine());
+               Console.WriteLine("{0}\n{1} {2}", t1, t2, t3);
+                for (int row=3; row <= n; ++row)
+                {
+                    for (int col=0; col<row; ++col)
+                    {
+                        tNum = t1+t2+t3;
+                        Console.Write("{0} ", tNum);
+                        t1 = t2;
+                        t2 = t3;
+                        t3 = tNum;
+                    }
+                    Console.WriteLine();
+                }
+        }
+    }
+}

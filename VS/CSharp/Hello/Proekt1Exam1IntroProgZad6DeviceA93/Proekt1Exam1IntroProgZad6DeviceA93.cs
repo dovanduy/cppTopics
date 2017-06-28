94461d25832b53e6c40c374fb2ff1695b3dfe3c7
kilarionov <k.ilarionov@gmail.com>
2017-06-28 22:38:57 +0300

    minor

--- VS/CSharp/Hello/Proekt1Exam1IntroProgZad6DeviceA93/Proekt1Exam1IntroProgZad6DeviceA93.cs
+++ VS/CSharp/Hello/Proekt1Exam1IntroProgZad6DeviceA93/Proekt1Exam1IntroProgZad6DeviceA93.cs
@@ -7,13 +7,15 @@ namespace Proekt1Exam1IntroProgZad6DeviceA93
         static void Main(string[] args)
         {
             long nSec = long.Parse(Console.ReadLine());
+            string result = "";
             for (long num=1, cntNum=0; nSec != 0; cntNum=0, ++num)
             {
                 for (; cntNum<num && nSec > 0; ++cntNum, --nSec)
                 {
-                    Console.Write("{0} ", num);
+                    result += num + " ";
                 }
             }
+            Console.Write(result);
         }
     }
 }

bc0a2ebcfa1836ff52e6cbca9f91065fc9525e96
kilarionov <k.ilarionov@gmail.com>
2017-06-28 19:15:46 +0300

    Proekt1Exam1Zad6DeviceA93

---
+++ VS/CSharp/Hello/Proekt1Exam1IntroProgZad6DeviceA93/Proekt1Exam1IntroProgZad6DeviceA93.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Proekt1Exam1IntroProgZad6DeviceA93
+{
+    class Proekt1Exam1IntroProgZad6DeviceA93
+    {
+        static void Main(string[] args)
+        {
+            long nSec = long.Parse(Console.ReadLine());
+            for (long num=1, cntNum=0; nSec != 0; cntNum=0, ++num)
+            {
+                for (; cntNum<num && nSec > 0; ++cntNum, --nSec)
+                {
+                    Console.Write("{0} ", num);
+                }
+            }
+        }
+    }
+}
